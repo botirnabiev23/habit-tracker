d134be020f3643ffe17fe8a90e1d54d873832665
botirnabiev23 <112731535+botirnabiev23@users.noreply.github.com>
2026-04-16 12:06:50 +0500

    feat: add create habit page

--- lib/presentation/create_habit/pages/create_habit_page.dart
+++ lib/presentation/create_habit/pages/create_habit_page.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:habit_tracker/core/router/route_paths.dart';
+import 'package:habit_tracker/domain/entities/habit.dart';
+import 'package:habit_tracker/presentation/create_habit/cubit/create_habit_cubit.dart';
 
-class CreateHabitPage extends StatelessWidget {
+class CreateHabitPage extends StatefulWidget {
   const CreateHabitPage({super.key});
 
   @override
+  State<CreateHabitPage> createState() => _CreateHabitPageState();
+}
+
+class _CreateHabitPageState extends State<CreateHabitPage> {
+  final textController = TextEditingController();
+  HabitFrequency selectedFrequency = HabitFrequency.daily;
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: BlocConsumer<CreateHabitCubit, CreateHabitState>(
+        listener: (context, state) {
+          if (state is CreateHabitSuccess) {
+            context.go(RoutePaths.habits);
+          }
+        },
+        builder: (context, state) {
+          final isLoading = state is CreateHabitLoading;
+          return Column(
+            children: [
+              if (state is CreateHabitError)
+                Text(state.message, style: TextStyle(color: Colors.red)),
+              TextField(controller: textController),
+              const SizedBox(height: 20),
+              DropdownButton<HabitFrequency>(
+                value: selectedFrequency,
+                items: HabitFrequency.values
+                    .map((f) => DropdownMenuItem(value: f, child: Text(f.name)))
+                    .toList(),
+                onChanged: (value) => setState(() => selectedFrequency = value!),
+              ),
+              const SizedBox(height: 20),
+              isLoading
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: () {
+                        context.read<CreateHabitCubit>().createHabit(
+                          textController.text,
+                          selectedFrequency,
+                        );
+                      },
+                      child: Text('Submit'),
+                    ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
