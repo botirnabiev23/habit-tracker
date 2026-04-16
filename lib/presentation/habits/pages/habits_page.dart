aaecf0326c5f71d906b668f0120205f6ec09b247
botirnabiev23 <112731535+botirnabiev23@users.noreply.github.com>
2026-04-16 11:42:44 +0500

    feat: add habits page

--- lib/presentation/habits/pages/habits_page.dart
+++ lib/presentation/habits/pages/habits_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_tracker/presentation/habits/bloc/habits_bloc.dart';
 
 class HabitsPage extends StatelessWidget {
   const HabitsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: BlocBuilder<HabitsBloc, HabitsState>(
+        builder: (context, state) {
+          if (state is LoadingHabitsState) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (state is LoadedHabitsState) {
+            return ListView.builder(
+              itemCount: state.habits.length,
+              itemBuilder: (context, index) {
+                final habit = state.habits[index];
+                return ListTile(title: Text(habit.name));
+              },
+            );
+          }
+          if (state is ErrorHabitsState) {
+            return Center(child: Text(state.message));
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
   }
 }
